0be29e92e2382dc01a8c281beaaf1c9945d42952
MingHaiZ <1163094232@qq.com>
2025-05-18 11:40:28 +0800

    <算法>2025/5/17

---
+++ src/main/java/leetCodeHot100/Test94.java
@@ -0,0 +1,48 @@
+package leetCodeHot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 递归中序遍历二叉树
+ */
+public class Test94 {
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    private static void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
